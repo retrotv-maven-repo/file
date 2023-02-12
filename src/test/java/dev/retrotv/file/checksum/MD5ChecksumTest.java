51ae0aceabf49e35e3ddfc1eb2a92d81c2cd291f
yjj8353 <yjj8353@gmail.com>
2023-02-13 01:25:01 +0900

    테스트 분할

---
+++ src/test/java/dev/retrotv/file/checksum/MD5ChecksumTest.java
@@ -0,0 +1,76 @@
+package dev.retrotv.file.checksum;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MD5ChecksumTest extends FileChecksumTest {
+
+    @Test
+    @DisplayName("MD5 hash 테스트")
+    void md5Hash() throws IOException {
+        File file;
+        try {
+            file = new File(Objects.requireNonNull(resource).toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        FileChecksum fc = new MD5Checksum();
+        String hash = fc.hash(file);
+
+        logger.debug("hash 값: " + hash);
+
+        assertNotNull(hash);
+    }
+
+    @Test
+    @DisplayName("MD5 hash file이 null인 경우 테스트")
+    void md5HashFileIsNull() {
+        FileChecksum fc = new MD5Checksum();
+        Throwable throwable = assertThrows(NullPointerException.class, () -> fc.hash(null));
+
+        assertEquals("파일 객체가 null 입니다.", throwable.getMessage());
+    }
+
+    @Test
+    @DisplayName("MD5 matches 테스트")
+    void md5Matches() throws IOException {
+        File file;
+        try {
+            file = new File(Objects.requireNonNull(resource).toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        FileChecksum fc = new MD5Checksum();
+        String checksum = fc.hash(file);
+
+        logger.debug("checksum: " + checksum);
+        assertNotNull(checksum);
+
+        boolean isSameFile = fc.matches(file, checksum);
+        logger.debug("same file: " + isSameFile);
+
+        assertTrue(isSameFile);
+
+        File file2;
+        try {
+            file2 = new File(Objects.requireNonNull(resource2).toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        isSameFile = fc.matches(file, file2);
+        logger.debug("same file: " + isSameFile);
+
+        assertTrue(isSameFile);
+    }
+}
---
+++ src/test/java/dev/retrotv/file/checksum/SHA384ChecksumTest.java
@@ -0,0 +1,76 @@
+package dev.retrotv.file.checksum;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SHA384ChecksumTest extends FileChecksumTest {
+
+    @Test
+    @DisplayName("SHA384 hash 테스트")
+    void sha384Hash() throws IOException {
+        File file;
+        try {
+            file = new File(Objects.requireNonNull(resource).toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        FileChecksum fc = new SHA512Checksum();
+        String hash = fc.hash(file);
+
+        logger.debug("hash 값: " + hash);
+
+        assertNotNull(hash);
+    }
+
+    @Test
+    @DisplayName("SHA384 hash file이 null인 경우 테스트")
+    void sha384HashFileIsNull() {
+        FileChecksum fc = new SHA384Checksum();
+        Throwable throwable = assertThrows(NullPointerException.class, () -> fc.hash(null));
+
+        assertEquals("파일 객체가 null 입니다.", throwable.getMessage());
+    }
+
+    @Test
+    @DisplayName("SHA384 matches 테스트")
+    void sha384Matches() throws IOException {
+        File file;
+        try {
+            file = new File(Objects.requireNonNull(resource).toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        FileChecksum fc = new SHA384Checksum();
+        String checksum = fc.hash(file);
+
+        logger.debug("checksum: " + checksum);
+        assertNotNull(checksum);
+
+        boolean isSameFile = fc.matches(file, checksum);
+        logger.debug("same file: " + isSameFile);
+
+        assertTrue(isSameFile);
+
+        File file2;
+        try {
+            file2 = new File(Objects.requireNonNull(resource2).toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        isSameFile = fc.matches(file, file2);
+        logger.debug("same file: " + isSameFile);
+
+        assertTrue(isSameFile);
+    }
+}
